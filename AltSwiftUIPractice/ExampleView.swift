457a703b3a8bf101ddaa9cd3a69291aa375ace78
yoshi104 <yoshiyukikt@gmail.com>
2020-10-30 01:52:23 +0900

    Add AltSwiftUI

---
+++ AltSwiftUIPractice/ExampleView.swift
@@ -0,0 +1,16 @@
+//
+//  ExampleView.swift
+//  AltSwiftUIPractice
+//
+//  Created by Yoshiyuki Kato on 2020/10/30.
+//
+
+import Foundation
+import AltSwiftUI
+
+struct ExampleView: View {
+    var viewStore = ViewValues()
+    var body: View {
+        EmptyView()
+    }
+}
